03a1b252183a69142110bce0194f1ba9bbb62884
tpo-tcmurphy <77013859+tpo-tcmurphy@users.noreply.github.com>
2021-03-29 14:22:54 -0400

    Added Course Routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const User = require('../models').User
+const Course = require('../models').Course
 const createError = require('http-errors')
 
 function asyncHandler (cb) {
@@ -31,5 +32,34 @@ router.post('/api/users', asyncHandler(async (req, res) => {
     res.status(400).json({ error })
   }
 }))
+router.get('/api/courses', asyncHandler(async (req, res) => {
+  const courses = await Course.findAll({
+    attributes: [
+      'title',
+      'description',
+      'estimatedTime',
+      'materialsNeeded'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: [
+          'firstName',
+          'lastName',
+          'emailAddress'
+        ]
+      }
+    ]
+  })
+  res.json(courses)
+}))
 
+router.post('/api/courses', asyncHandler(async (req, res) => {
+  try {
+    await Course.create(req.body)
+    res.location('/').status(201).json(req.body)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+}))
 module.exports = router
